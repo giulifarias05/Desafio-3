e20a12f9a9d63b367fa190345ff3302fa3ab2a75
giulifarias05 <88504587+giulifarias05@users.noreply.github.com>
2023-09-20 14:00:46 -0300

    primeros cambios

--- desafio3/src/App.js
+++ desafio3/src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [descripcion , setDescripcion]= useState('');
+
+  function Cambios(e){
+    e.preventDefault()
+    console.log(descripcion)
+  }
+
   return (
     <div className="App">
       <h1>Desafio 3</h1>
+      <form>
+        <label> ingrese descripcion</label>
+        <input onChange={(e) => setDescripcion(e.target.value)}/>    
+        <br></br>
+        <button onClick={Cambios}>Cargar ingrediente</button>
+      </form>
     </div>
   );
 }
